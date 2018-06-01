0f439c90a263167a0bc6798ad20a1e0f0d175333
Mitchell Douglass <mrdoug95@stanford.edu>
2018-06-01 20:08:23 +0000

    add code to precompute flow and acceleration

--- flow.py
+++ flow.py
@@ -1,21 +1,84 @@
 import os, sys
+from threading import Thread
+import time
 
 import cv2
 import numpy as np
+from queue import Queue
+
+
+class FileVideoStream:
+    def __init__(self, path, queueSize=90):
+        # initialize the file video stream along with the boolean
+        # used to indicate if the thread should be stopped or not
+        self.stream = cv2.VideoCapture(path)
+        self.stopped = False
+ 
+        # initialize the queue used to store frames read from
+        # the video file
+        self.Q = Queue(maxsize=queueSize)
+
+    def start(self):
+        # start a thread to read frames from the file video stream
+        t = Thread(target=self.update, args=())
+        t.daemon = True
+        t.start()
+        return self
+
+    def update(self):
+        # keep looping infinitely
+        while True:
+            # if the thread indicator variable is set, stop the
+            # thread
+            if self.stopped:
+                return
+ 
+            # otherwise, ensure the queue has room in it
+            if not self.Q.full():
+                # read the next frame from the file
+                (grabbed, frame) = self.stream.read()
+ 
+                # if the `grabbed` boolean is `False`, then we have
+                # reached the end of the video file
+                if not grabbed:
+                    self.stop()
+                    return
+ 
+                # add the frame to the queue
+                self.Q.put(frame)
+
+    def read(self):
+        # return next frame in the queue
+        while self.Q.qsize() == 0:
+            time.sleep(0.001)
+        return self.Q.get()
+
+    def more(self):
+        # return True if there are still frames in the queue
+        return not self.stopped
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
+        self.stream.release()
 
 def compute_flow_for_video(video_fn):
+    def avg(fm):
+        return (fm[::2,::2] + fm[1::2,1::2]) / 2.
     flows = []
-    cap = cv2.VideoCapture(video_fn)
-    ret, frame1 = cap.read()
-    prvs = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    ret, frame2 = cap.read()
-    while ret:
-        next = cvs.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    fvs = FileVideoStream(video_fn).start()
+    frame1 = fvs.read()
+    prvs = avg(cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY))
+    while fvs.more():
+        frame2 = fvs.read()
+        next = avg(cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY))
         flow = cv2.calcOpticalFlowFarneback(
-                prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+                prvs, next, None, 0.5, 3, 8, 2, 5, 1.2,
+                cv2.OPTFLOW_USE_INITIAL_FLOW)
         flows.append(flow)
-        ret, frame2 = cap.read()
         prvs = next
+    fvs.stop()
+    cv2.destroyAllWindows()
     # assume motion continues
     flows.append(flows[-1])
     accels = []
@@ -23,7 +86,7 @@ def compute_flow_for_video(video_fn):
         next = flows[i+1]
         accels.append(next-flow)
     accels.append(accels[-1])
-    result = [np.concat([f, a], axis=2) for f, a in zip(flows, accels)]
+    result = [np.concatenate([f, a], axis=2) for f, a in zip(flows, accels)]
     return result
 
 root = None
@@ -44,14 +107,14 @@ def process_split(split):
                 fn, label, _, _ = line.split(',')
                 assert (fn[-4:] == '.mp4')
                 frames = compute_flow_for_video(f'{root}{split}/{fn}')
-                for j, frame in data():
-                    frame_fn = f'flow_accel_{j}.npy'
+                for j, frame in enumerate(frames):
+                    frame_fn = f'flow_{j+1:05}.npy'
                     if not os.path.isdir(f'{root}{split}/{fn[:-4]}/{frame_fn}'):
                         if backup is not None and \
                                 os.path.isdir(f'{backup}{split}/{fn[:-4]}/{frame_fn}'):
                             os.system(f'cp {backup}{split}/{fn[:-4]}/{frame_fn} {root}{split}/{fn[:-4]}/{frame_fn}')
                         else:
-                            np.save(f'{root}{split}/{fn[:-4]/{frame_fn}', frame)
+                            np.save(f'{root}{split}/{fn[:-4]}/{frame_fn}', frame)
                 i += 1
                 print('%5d: %s' % (i, fn))
 
